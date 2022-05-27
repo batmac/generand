532c6f14482cec94697cf1f5c8166b3a2f47165a
Baptiste Canton <bat@sbz.fr>
2022-05-27 14:15:48 +0200

    add -n

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 use generand::{consts::*, generate::*};
 
+use terminal_size::{terminal_size, Width};
+
+use std::cmp;
+
 use clap::Parser;
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    #[clap(long, short, default_value="12")]
+    #[clap(long, short, default_value = "12")]
     size: usize,
-    #[clap(long, short, group="dict")]
+    #[clap(long, short, group = "dict")]
     full: bool,
-    #[clap(long, short, group="dict")]
+    #[clap(long, short, group = "dict")]
     cats: bool,
-    #[clap(long, short, group="dict")]
+    #[clap(long, short, group = "dict")]
     hex: bool,
-    #[clap(long, short, group="dict")]
-    numeric: bool,
-    #[clap(group="dict")]
-    dictionary: Option<String>, 
+    #[clap(long, short, group = "dict")]
+    digits: bool,
+    #[clap(long, short, default_value = "1")]
+    number: usize,
+    #[clap(group = "dict")]
+    dictionary: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let s= if let Some(x)=cli.dictionary {
+    let s = if let Some(x) = cli.dictionary {
         x
     } else if cli.full {
         SEVENBITS.to_string()
@@ -29,12 +35,32 @@ fn main() {
         CATS.to_string()
     } else if cli.hex {
         LOW_HEX.to_string()
-    } else if cli.numeric {
+    } else if cli.digits {
         NUMERIC.to_string()
     } else {
         ALPHANUMERIC.to_string()
     };
 
-    let generated = generate(&s, cli.size);
-    println!("{}", &generated);
+    let term_width = if let Some((Width(w), _)) = terminal_size() {
+        w as usize
+    } else {
+        1
+    };
+
+    let max_per_line = cmp::max(1, term_width / (cli.size + 1));
+    eprintln!("max per line {max_per_line}");
+
+    let mut to_display = cli.number;
+    while to_display > 0 {
+        //eprintln!("to display: {to_display}");
+        for _ in 0..max_per_line {
+            let generated = generate(&s, cli.size);
+            print!("{} ", &generated);
+            to_display -= 1;
+            if to_display == 0 {
+                break;
+            }
+        }
+        println!()
+    }
 }
